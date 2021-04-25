28138dac8d2fea6df44a5a4e69555aa6527710e7
WashinChuen <73007799+WashinChuen@users.noreply.github.com>
2021-04-25 18:55:33 +0900

    Unofficial Demo Converted to Unity Version 2020.2.5f1
    Unity files for Firewall Invader. Most recent change: Changed Unity Version, Resized Everything, Double Click Icons, Player Modes, Player Follows Mouse, Created Many Prefabs

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField]
+    float bulletSpeed;
+    private float secondsLeft = 3f;
+
+    private void Awake()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = -transform.up * bulletSpeed;
+    }
+
+    void Update()
+    {
+        secondsLeft -= Time.deltaTime;
+        if (secondsLeft <= 0) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DestroyPlayerScript _destroyPlayerScript = collision.gameObject.GetComponent<DestroyPlayerScript>();
+            _destroyPlayerScript.GotHit();
+        }
+        Destroy(this.gameObject);
+    }
+
+}
