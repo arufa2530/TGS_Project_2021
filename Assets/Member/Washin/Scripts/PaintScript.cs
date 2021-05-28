941d48034a0bf64219141bc7b038bdf26e4a0008
WashinChuen <73007799+WashinChuen@users.noreply.github.com>
2021-05-28 13:16:25 +0900

    New Brush testing

---
+++ Assets/Member/Washin/Scripts/PaintScript.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System;
+
+public class PaintScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject defaultBrush;
+    [SerializeField]
+    Transform drawSpace;
+    [SerializeField]
+    Transform brushHolder;
+    [SerializeField]
+    RenderTexture renderedTexture;
+    int imageCount = 0;
+    public Vector3 brushScaleValue;
+
+    [SerializeField]
+    Slider brushScaleSlider;
+
+    [SerializeField]
+    IsMouseWithinBoundriesScript _IsMouseWithinBoundriesScript;
+
+    [SerializeField]
+    Sprite stockImage;
+    Texture2D[] images;
+    int x, y;
+
+    GameObject displayForCurrentBrush;
+    Image brushColorRenderer;
+    Color currentSelectedColor = Color.yellow;
+    RaycastHit2D hitForBrush;
+
+    bool shouldBrushBeShowing;
+
+    private void Start()
+    {
+        displayForCurrentBrush = Instantiate(defaultBrush, brushHolder);
+        brushColorRenderer = displayForCurrentBrush.GetComponent<Image>();
+    }
+
+
+    private void Update()
+    {
+        if (!_IsMouseWithinBoundriesScript.isMouseInsideDrawBoundries())
+        {
+            brushColorRenderer.color = Color.red;
+            displayForCurrentBrush.transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            brushColorRenderer.color = currentSelectedColor;
+            hitForBrush = MousePositionFromRay();
+            displayForCurrentBrush.transform.position = hitForBrush.point;
+            Draw();
+        }
+        displayForCurrentBrush.transform.localScale = ChangeBrushSize();
+    }
+
+
+
+    private void Draw()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //RaycastHit2D hit = MousePositionFromRay();
+
+            GameObject brushStroke = Instantiate(defaultBrush, hitForBrush.point, Quaternion.identity, drawSpace);
+            brushStroke.transform.localScale = ChangeBrushSize();
+        }
+    }
+
+    private static RaycastHit2D MousePositionFromRay()
+    {
+        Vector3 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(ray.x, ray.y), Vector2.zero);
+        return hit;
+    }
+
+    public void Save()
+    {
+        shouldBrushBeShowing = false;
+        //DisplayPreviewBrush();
+        StartCoroutine(SaveRenderTextureToPng());
+        shouldBrushBeShowing = true;
+        //DisplayPreviewBrush();
+    }
+
+
+    private IEnumerator SaveRenderTextureToPng()
+    {
+        yield return new WaitForEndOfFrame();
+
+        RenderTexture.active = renderedTexture;
+
+        var drawnImage = new Texture2D(renderedTexture.width, renderedTexture.height);
+        drawnImage.ReadPixels(new Rect(0, 0, renderedTexture.width, renderedTexture.height), 0, 0);
+        drawnImage.Apply();
+
+        var drawningToSave = drawnImage.EncodeToPNG();
+        //File.WriteAllBytes(Application.dataPath + "/Member/Washin/Images/PaintedImages/Image-" + System.Guid.NewGuid() + ".png", drawningToSave);
+        File.WriteAllBytes(Application.dataPath + "/Member/Washin/Images/PaintedImages/Image-" + ++imageCount + ".png", drawningToSave);
+    }
+
+    public void CompareTwoDrawings()
+    {
+        images = new Texture2D[2];
+        images[0] = new Texture2D(1, 1);
+        images[1] = new Texture2D(1, 1);
+
+        images[0].LoadImage(File.ReadAllBytes(Application.dataPath + "/Member/Washin/Images/PaintedImages/Image-" + imageCount + ".png"));
+        images[1].LoadImage(File.ReadAllBytes(AssetDatabase.GetAssetPath(stockImage)));
+
+
+        for (x = images[0].width; x > 0; x--)
+        {
+            for (y = images[0].height; y > 0; y--)
+            {
+
+            }
+        }
+    }
+
+    public Vector3 ChangeBrushSize()
+    {
+        float tempScale = brushScaleSlider.value;
+        brushScaleValue = new Vector3(tempScale, tempScale, tempScale);
+        return brushScaleValue;
+    }
+
+    public void DisplayPreviewBrush()
+    {
+        if (shouldBrushBeShowing)
+            displayForCurrentBrush.SetActive(shouldBrushBeShowing);
+        else
+            displayForCurrentBrush.SetActive(!shouldBrushBeShowing);
+    }
+
+
+}
