9bfb421b08683dfa218270edbab294c71dfaef39
hayashi_hayato <zieghurito@gmail.com>
2021-09-17 15:43:37 +0900

    バナー修正、UI移動スクリプトの細かい修正

--- Assets/Member/Hayashi/Script/MyComSc/DragMoveUI.cs
+++ Assets/Member/Hayashi/Script/MyComSc/DragMoveUI.cs
@@ -5,38 +5,37 @@ using UnityEngine.UI;
 
 public class DragMoveUI : MonoBehaviour
 {
-    Vector3 MousePos;
+    Vector3 mousePos;
 
     private void OnMouseDown()
     {
-        Debug.Log(this.transform.position);
-        Vector2 mouse = RectTransformUtility.WorldToScreenPoint(Camera.main, this.GetComponent<RectTransform>().position);
-        MousePos = new Vector3(mouse.x - Input.mousePosition.x, mouse.y - Input.mousePosition.y, 0f);
-        Debug.Log(MousePos);
+        // Objectの中心座標とマウスの座標との差分を計算、MousePosに格納
+        Vector2 obj = RectTransformUtility.WorldToScreenPoint(Camera.main, this.GetComponent<RectTransform>().position);
+        mousePos = new Vector3(obj.x - Input.mousePosition.x, obj.y - Input.mousePosition.y, 0f);;
     }
 
     private void OnMouseUp()
     {
-        MousePos = Vector3.zero;
+        mousePos = Vector3.zero;
     }
 
     private void OnMouseDrag()
     {
-        //Cubeの位置をワールド座標からスクリーン座標に変換して、objectPointに格納
+        //objectの位置をワールド座標からスクリーン座標に変換して、objectPointに格納
         Vector3 objectPoint
             = Camera.main.WorldToScreenPoint(transform.position);
 
-        //Cubeの現在位置(マウス位置)を、pointScreenに格納
+        //マウスの現在位置をpointScreenに格納
         Vector3 pointScreen
             = new Vector3(Input.mousePosition.x,
                           Input.mousePosition.y,
-                          objectPoint.z) + MousePos;
+                          objectPoint.z) + mousePos;
 
-        //Cubeの現在位置を、スクリーン座標からワールド座標に変換して、pointWorldに格納
+        //pointScreenを、スクリーン座標からワールド座標に変換して、pointWorldに格納
         Vector3 pointWorld = Camera.main.ScreenToWorldPoint(pointScreen);
         pointWorld.z = transform.position.z;
 
-        //Cubeの位置を、pointWorldにする
+        //Objectの位置を、pointWorldにする
         transform.position = pointWorld;
     }
 }
